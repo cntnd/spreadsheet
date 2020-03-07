a91d0c0111425945786583562dbeb48fda702e88
Thomas D'Ascoli <thomas@dasco.li>
2020-03-07 14:29:23 +0100

    fix

--- src/php/includes/style.cntnd_simple_spreadsheet_input.php
+++ src/php/includes/style.cntnd_simple_spreadsheet_input.php
@@ -1,7 +1,3 @@
-
-<link rel="stylesheet" href="../../css/cntnd_simple_spreadsheet.css" type="text/css" />
-<!--
 <style>
 <?= file_get_contents($cfgClient[$client]["module"]["path"].'cntnd_simple_spreadsheet/css/cntnd_simple_spreadsheet.css') ?>
 </style>
--->
